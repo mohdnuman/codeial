121b19db61474fa6e836ae2669d8f3972e5ae05f
Mohd Numan <mohammadnuman123@gmail.com>
2021-09-29 19:54:49 +0530

    added the view engine

---
+++ controllers/posts_controller.js
@@ -0,0 +1,3 @@
+module.exports.show=function(req,res){
+    return res.end('<h1>post image</h1>');
+}
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const port=8000;
 const app=express();
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.use('/',require('./routes/index.js'));
 
 app.listen(port,function(err){

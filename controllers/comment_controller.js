50e2477bd52fb24c35083f9865849770d7cb1c08
Mohd Numan <mohammadnuman123@gmail.com>
2021-10-07 17:26:55 +0530

    added feature to delete comments and posts

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,3 +1,4 @@
+const { modelNames } = require('mongoose');
 const Comment=require('../models/comment');
 const Post=require('../models/post');
 
@@ -24,4 +25,25 @@ module.exports.create=function(req,res){
             });
         }
     })
+}
+
+module.exports.destroy=function(req,res){
+    Comment.findById(req.params.id,function(err,comment){
+        if(err){
+            console.log("error occurred while finding comment to delete");
+            return;
+        }
+        if(comment.user==req.user.id)
+        {
+            let postid=comment.post;
+            comment.remove();
+
+            Post.findByIdAndUpdate(postid, {$pull:{comments:req.params.id}},function(err,post){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    })
+
 }

00dd3b5c1f89b0713a654e6caeba7b5bc0aebdd6
Mohd Numan <mohammadnuman123@gmail.com>
2021-11-08 22:37:31 +0530

    added view-helpers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const cookieParser=require('cookie-parser');
 const app=express();
+require('./config/view-helpers')(app);
 const port=8000;
 const db=require('./config/mongoose');
 const env=require('./config/environment');
